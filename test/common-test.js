fc848fe7a55d590b30b3d9d7edc953ab150676fb
yawnt <yawn.localhost@gmail.com>
2012-06-28 11:21:04 +0200

    test #use()

--- test/common-test.js
+++ test/common-test.js
@@ -1,9 +1,27 @@
-var zen = require('../');
+var zen  = require('../');
 
 describe('zen', function() {
   describe('#use()', function() {
-    it('should be true', function() {
-      (1).should.equal(1);
+    it('should point to http://127.0.0.1:8098 if `undefined` is passed', function() {
+      zen.use();
+      
+      zen._proto.should.equal('http:');
+      zen._host.should.equal('127.0.0.1');
+      zen._port.should.equal('8098')
+    });
+    it('should point to http://localhost:8098 is `localhost` is passed', function() {
+      zen.use('http://localhost');
+
+      zen._proto.should.equal('http:');
+      zen._host.should.equal('localhost');
+      zen._port.should.equal('8098')
+    });
+    it('should point to ftp://foobar:1234 if `ftp://foobar:1234` is passed', function() {
+      zen.use('ftp://foobar:1234');
+
+      zen._proto.should.equal('ftp:');
+      zen._host.should.equal('foobar');
+      zen._port.should.equal('1234')     
     });
   })
 });
